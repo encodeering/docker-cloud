9539831671f6ebf309fe569f07d8ae5ceb59c7ff
Michael Clausen <encodeering@gmail.com>
2016-01-04 16:41:46 +0100

    container should be executed once
    * and print the owncloud version; can be seen as verification and debug/audit step
    
    NOTE
    
    * entrypoint calls cat instead of apache2-foreground here after everthing has setup

--- .travis/build.sh
+++ .travis/build.sh
@@ -9,3 +9,5 @@ docker pull   "$REPOSITORY/php-$ARCH:5.6-$VARIANT"
 docker tag -f "$REPOSITORY/php-$ARCH:5.6-$VARIANT" "php:5.6-$VARIANT"
 
 docker build -t "$TAG:$TAGSPECIFIER" "$PROJECT/$VERSION/$VARIANT"
+
+docker run --rm "$TAG:$TAGSPECIFIER" cat version.php
