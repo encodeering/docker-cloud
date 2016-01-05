6dd4ed967168f67b49e681aef8b3a32f93a40a57
Michael Clausen <encodeering@gmail.com>
2016-01-05 12:30:40 +0100

    creates a debian based owncloud image
    * arch: armhf
    * version: 8.2-apache
    
    NOTE
    
    * currently, all official apache variants use version 5.6; no need to make it variable yet

---
+++ .travis/build.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+set -ev
+
+TAG="$REPOSITORY/$PROJECT-$ARCH"
+TAGSPECIFIER="$VERSION-$VARIANT"
+
+docker pull   "$REPOSITORY/php-$ARCH:5.6-$VARIANT"
+docker tag -f "$REPOSITORY/php-$ARCH:5.6-$VARIANT" "php:5.6-$VARIANT"
+
+docker build -t "$TAG:$TAGSPECIFIER" "$PROJECT/$VERSION/$VARIANT"
