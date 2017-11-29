8a6a81c3aec8ffb8de45fe0a4fb4e53724cd3fc2
Michael Clausen <encodeering@gmail.com>
2017-11-29 14:43:57 +0100

    removes ssl split
    * armhf will now be completely build by shippable, so there's no need for a splitted build anymore

--- modules/build.sh
+++ modules/build.sh
@@ -13,24 +13,12 @@ import com.encodeering.docker.docker
 
 SEMANTIC="${VERSION%.*}"
 
-case "$CUSTOM" in
-    ssl )
-        docker-pull "$DOCKER_IMAGE" "$PROJECT:$VARIANT"
+docker-pull "$REPOSITORY/php-$ARCH:$VARIANTVERSION-$VARIANT" "php:$VARIANT"
 
-        [ -d "owncloud-ssl/$VARIANT" ] || exit 1
+patch -p1 --no-backup-if-mismatch --directory="$PROJECT" < "patch/$SEMANTIC/$VARIANT/Dockerfile.patch"
 
-        docker build -t "$DOCKER_IMAGE-$CUSTOM" "owncloud-ssl/$VARIANT"
+docker build -t "$DOCKER_IMAGE" -t "$PROJECT:$VARIANT" --build-arg OWNCLOUD_VERSION="$VERSION" "$PROJECT/$SEMANTIC/$VARIANT"
+docker build -t "$DOCKER_IMAGE-ssl" "owncloud-ssl/$VARIANT"
 
-        docker run --rm "$DOCKER_IMAGE-$CUSTOM" cat version.php
-        ;;
-    * )
-        docker-pull "$REPOSITORY/php-$ARCH:$VARIANTVERSION-$VARIANT" "php:$VARIANT"
-
-        patch -p1 --no-backup-if-mismatch --directory="$PROJECT" < "patch/$SEMANTIC/$VARIANT/Dockerfile.patch"
-
-        docker build -t "$DOCKER_IMAGE" --build-arg OWNCLOUD_VERSION="$VERSION" "$PROJECT/$SEMANTIC/$VARIANT"
-
-        docker run --rm "$DOCKER_IMAGE" cat version.php
-        ;;
-esac
-
+docker run --rm "$DOCKER_IMAGE"     cat version.php
+docker run --rm "$DOCKER_IMAGE-ssl" cat version.php
