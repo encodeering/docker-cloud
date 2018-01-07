097bf5ceebaf6fca54d2ea008a446476fb6a9a16
Michael Clausen <encodeering@gmail.com>
2018-01-07 14:45:33 +0100

    removes variables from build
    * variant version is already pinned by the commit

--- modules/build.sh
+++ modules/build.sh
@@ -11,9 +11,9 @@ set -e
 import com.encodeering.docker.config
 import com.encodeering.docker.docker
 
+docker-pull "$REPOSITORY/php-$ARCH:7.0-$BASE-$VARIANT" "php:7.0-$VARIANT"
 SEMANTIC="${VERSION%.*}"
 
-docker-pull "$REPOSITORY/php-$ARCH:$VARIANTVERSION-$BASE-$VARIANT" "php:$VARIANTVERSION-$VARIANT"
 
 docker build -t "$DOCKER_IMAGE" -t "$PROJECT:$VARIANT" "$PROJECT/$SEMANTIC/$VARIANT"
 docker build -t "$DOCKER_IMAGE-ssl" "owncloud-ssl/$VARIANT"
