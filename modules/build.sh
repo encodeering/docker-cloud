f7adc1ff445dd899da1685a346c51493cfbfc037
Michael Clausen <encodeering@gmail.com>
2018-01-07 14:48:52 +0100

    removes micro version from image
    * micro patches should be implicitly applied
    
    NOTE
    
    * owncloud honors minor changes, compare with 9.x

--- modules/build.sh
+++ modules/build.sh
@@ -12,10 +12,8 @@ import com.encodeering.docker.config
 import com.encodeering.docker.docker
 
 docker-pull "$REPOSITORY/php-$ARCH:7.0-$BASE-$VARIANT" "php:7.0-$VARIANT"
-SEMANTIC="${VERSION%.*}"
 
-
-docker build -t "$DOCKER_IMAGE" -t "$PROJECT:$VARIANT" "$PROJECT/$SEMANTIC/$VARIANT"
+docker build -t "$DOCKER_IMAGE" -t "$PROJECT:$VARIANT" "$PROJECT/$VERSION/$VARIANT"
 docker build -t "$DOCKER_IMAGE-ssl" "owncloud-ssl/$VARIANT"
 
 docker run --rm "$DOCKER_IMAGE"     cat version.php
