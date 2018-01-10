851d919f7aeb052be5eedbaf3ceef8b5a5c9b592
Michael Clausen <encodeering@gmail.com>
2018-01-10 11:56:59 +0100

    updates build files to match ci package 0.1.7
    * build and verification shouldn't use the hard coded DOCKER_IMAGE value
    * package import changes

--- modules/build.sh
+++ modules/build.sh
@@ -2,13 +2,13 @@
 
 set -e
 
-import com.encodeering.docker.config
-import com.encodeering.docker.docker
+import com.encodeering.ci.config
+import com.encodeering.ci.docker
 
 docker-pull "$REPOSITORY/php-$ARCH:7.0-$BASE-$VARIANT" "php:7.0-$VARIANT"
 
-docker build -t "$DOCKER_IMAGE" -t "$PROJECT:$VARIANT" "$PROJECT/$VERSION/$VARIANT"
-docker build -t "$DOCKER_IMAGE-ssl" "owncloud-ssl/$VARIANT"
+docker-build -t "$PROJECT:$VARIANT" "$PROJECT/$VERSION/$VARIANT"
+docker-build --suffix ssl "owncloud-ssl/$VARIANT"
 
-docker run --rm "$DOCKER_IMAGE"     cat version.php
-docker run --rm "$DOCKER_IMAGE-ssl" cat version.php
+docker-verify              cat version.php
+docker-verify --suffix ssl cat version.php
