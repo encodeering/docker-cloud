88f95567beefee23c66fc1e52bea29a37ebbb8eb
Michael Clausen <encodeering@gmail.com>
2018-03-30 19:34:55 +0200

    enables vhost at runtime
    * ssl should be preferred if not used in conjunction with a reverse proxy
    
    NOTE
    
    * cli arguments should be reserved for cloud commands, so we use an env var to determine the vhost

--- modules/owncloud-ssl/apache/bin/docker-entrypoint.sh
+++ modules/owncloud-ssl/apache/bin/docker-entrypoint.sh
@@ -1,5 +1,13 @@
 #!/usr/bin/env bash
 
 set -e
+set -o pipefail
+
+if [ `find ${APACHE_CONFDIR}/sites-enabled ! -name "${VHOST}.*" -type l | wc -l` -gt 0 ]; then
+    echo "error: a vhost has already been enabled - VHOST envvar should be treated as effectively immutable"
+    exit 1
+fi
+
+a2ensite ${VHOST}
 
 exec docker-owncloud.sh "$@"
