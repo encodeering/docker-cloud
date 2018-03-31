a1a9b6604ab4dd452d1ff5623c6d96d2adaf5dee
Michael Clausen <encodeering@gmail.com>
2018-03-31 18:57:01 +0200

    moves apache to a dedicate sub directory
    * like to introduce a variant for fpm using nginx next and it seems to be easier to avoid conditional build scripts
    
    NOTE
    
    * root project may later contain a cron image and we could generalize this project to handle nextcloud as well

--- modules/build.sh
+++ modules/build.sh
@@ -2,13 +2,4 @@
 
 set -e
 
-import com.encodeering.ci.config
-import com.encodeering.ci.docker
-
-docker-pull "$REPOSITORY/php-$ARCH:7.0-$BASE-$VARIANT" "php:7.0-$VARIANT"
-
-docker-build -t "$PROJECT:$VARIANT" "$PROJECT/$VERSION/$VARIANT"
-docker-build --suffix ssl "owncloud-ssl/$VARIANT"
-
-docker-verify              cat version.php
-docker-verify --suffix ssl cat version.php
+(cd "$VARIANT" && "./build.sh")
