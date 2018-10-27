ff9d5080d4f67eb0ec028c7ddfc76ed8deaa9ec7
Michael Clausen <encodeering@gmail.com>
2018-10-27 11:24:31 +0200

    fixes an issue with wrong base images for nextcloud
    * previous update commit requires a docker tag for php 7.2, which hasn't been provided, and loaded from dockerhub (amd64)
    ** working image for amd64, but broken for armhf - even though the base is wrong for both images

--- modules/fpm/build.sh
+++ modules/fpm/build.sh
@@ -6,6 +6,7 @@ import com.encodeering.ci.config
 import com.encodeering.ci.docker
 
 docker-pull "$REPOSITORY/php-$ARCH:7.0-$BASE-$VARIANT" "php:7.0-$VARIANT" "php:7.1-$VARIANT"
+docker-pull "$REPOSITORY/php-$ARCH:7.2-$BASE-$VARIANT" "php:7.2-$VARIANT" "php:7.2-$VARIANT-stretch"
 
 docker-build -t "cloud:$VARIANT" "$PROJECT/$VERSION/$VARIANT"
 docker-build --suffix sequel sequel
