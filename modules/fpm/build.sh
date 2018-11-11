2d37c00be8bde53d51dd4952a8a0735266435973
Michael Clausen <encodeering@gmail.com>
2018-11-11 15:55:45 +0100

    updates nextcloud and owncloud to latest release
    * owncloud 10.0 is based on 7.2 now
    * nextcloud uses an rsync step in between

--- modules/fpm/build.sh
+++ modules/fpm/build.sh
@@ -5,11 +5,18 @@ set -e
 import com.encodeering.ci.config
 import com.encodeering.ci.docker
 
-docker-pull "$REPOSITORY/php-$ARCH:7.0-$BASE-$VARIANT" "php:7.0-$VARIANT" "php:7.1-$VARIANT"
 docker-pull "$REPOSITORY/php-$ARCH:7.2-$BASE-$VARIANT" "php:7.2-$VARIANT" "php:7.2-$VARIANT-stretch"
 
 docker-build -t "cloud:$VARIANT" "$PROJECT/$VERSION/$VARIANT"
 docker-build --suffix sequel sequel
 
-docker-verify                 cat version.php
-docker-verify --suffix sequel cat version.php
+case "$PROJECT" in
+  owncloud)
+    docker-verify                 cat version.php
+    docker-verify --suffix sequel cat version.php
+    ;;
+  nextcloud)
+    docker-verify                 cat /usr/src/nextcloud/version.php
+    docker-verify --suffix sequel cat /usr/src/nextcloud/version.php
+    ;;
+esac
