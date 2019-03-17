10f0d0ef46d4f24f71cd4d0b7d6cac75fb015d8a
Michael Clausen <encodeering@gmail.com>
2019-03-17 13:45:26 +0100

    verifies if the binary responds with a correct image version
    * image tag should match the version of the binary

--- modules/fpm/build.sh
+++ modules/fpm/build.sh
@@ -10,13 +10,17 @@ docker-pull "$REPOSITORY/php-$ARCH:7.2-$BASE-$VARIANT" "php:7.2-$VARIANT" "php:7
 docker-build -t "cloud:$VARIANT" "$PROJECT/$VERSION/$VARIANT"
 docker-build --suffix sequel sequel
 
+check () {
+    dup | contains "VersionString = '${VERSION}"
+}
+
 case "$PROJECT" in
   owncloud)
-    docker-verify                 cat version.php
-    docker-verify --suffix sequel cat version.php
+    docker-verify                 cat version.php | check
+    docker-verify --suffix sequel cat version.php | check
     ;;
   nextcloud)
-    docker-verify                 cat /usr/src/nextcloud/version.php
-    docker-verify --suffix sequel cat /usr/src/nextcloud/version.php
+    docker-verify                 cat /usr/src/nextcloud/version.php | check
+    docker-verify --suffix sequel cat /usr/src/nextcloud/version.php | check
     ;;
 esac
