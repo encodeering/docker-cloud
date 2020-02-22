cec39dbcc2062c0ad66f6600d5a15fc2b3f2774f
Michael Clausen <encodeering@gmail.com>
2020-02-22 18:15:03 +0100

    updating scm repository to latest revision
    * 4cd608a6e077168529b5501aa1c1ae3df4f9c505

--- modules/fpm/build.sh
+++ modules/fpm/build.sh
@@ -6,7 +6,7 @@ import com.encodeering.ci.config
 import com.encodeering.ci.docker
 
 docker-pull "$REPOSITORY/php-$ARCH:7.2-$BASE-$VARIANT" "php:7.2-$VARIANT"
-docker-pull "$REPOSITORY/php-$ARCH:7.3-$BASE-$VARIANT" "php:7.3-$VARIANT-stretch"
+docker-pull "$REPOSITORY/php-$ARCH:7.3-$BASE-$VARIANT" "php:7.3-$VARIANT-buster"
 
 docker-build -t "cloud:$VARIANT" "$PROJECT/$VERSION/$VARIANT"
 docker-build --suffix sequel sequel
