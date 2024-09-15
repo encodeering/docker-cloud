2bd739167f795967c33b1cf613a0b291ac3a8195
Michael Clausen <encodeering@gmail.com>
2024-09-15 11:29:25 +0200

    updates nextcloud to the latest version
    * https://github.com/nextcloud/docker/commit/0ffd24170f863f302dfb74fe3a63b6c41f82a4fa
    
    NOTE
    
    * removes additional configurations which are part of the base image; allowing us to use the sequel also for alpine based images

--- modules/fpm/build.sh
+++ modules/fpm/build.sh
@@ -5,11 +5,7 @@ set -e
 import com.encodeering.ci.config
 import com.encodeering.ci.docker
 
-docker-pull "$REPOSITORY/php-$ARCH:7.2-$BASE-$VARIANT" "php:7.2-$VARIANT"
-docker-pull "$REPOSITORY/php-$ARCH:7.3-$BASE-$VARIANT" "php:7.3-$VARIANT-buster"
-
-docker-build -t "cloud:$VARIANT" "$PROJECT/$VERSION/$VARIANT"
-docker-build --suffix sequel sequel
+./build-${BASE}.sh
 
 check () {
     dup | contains "VersionString = '${VERSION}"
--- modules/fpm/sequel/bin/docker-entrypoint.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-set -e
-set -o pipefail
-
-exec docker-cloud.sh "$@"
